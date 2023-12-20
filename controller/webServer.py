361698dfc65f7c9fbf1b36fd644d7b8fb7dc851d
MariaBLSR <83512905+MariaBLSR@users.noreply.github.com>
2023-12-21 00:17:05 +0100

    Merge remote-tracking branch 'origin/main'

--- controller/webServer.py
+++ controller/webServer.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from .LibraryController import LibraryController
 from flask import Flask, render_template, request, make_response, redirect
 from datetime import datetime
@@ -78,7 +80,53 @@ def perfil():
 	else:
 		user = library.get_user_id(userId)
 
-	return render_template('perfil.html',user=user)
+	#Obtener los libros en reserva del usuario actual
+	con = sqlite3.connect('datos/datos.db')
+	cur = con.cursor()
+	cur.execute("""
+		SELECT Book.title, Author.name
+		FROM Reserva
+		JOIN CopiaLibro ON Reserva.IdCopiaLibro = CopiaLibro.IdCopia
+		JOIN Book ON CopiaLibro.LibroIdLibro = Book.id
+		JOIN Author ON Book.author = Author.id
+		WHERE Reserva.UsuarioIdU = ?
+	""", (user.id,))
+
+	libros_en_reserva = cur.fetchall()
+
+	#Estado libro a devuelto
+	if request.method == 'POST':
+		libro_devuelto = request.form.get('libro_devuelto')
+		cur.execute("""
+			UPDATE Reserva
+			SET devuelto = 1
+			WHERE IdCopiaLibro = ? AND UsuarioIdU = ?
+	""", (user.id,))
+
+	cur.execute("""
+		SELECT Book.title, Author.name
+		FROM Reserva
+		JOIN CopiaLibro ON Reserva.IdCopiaLibro = CopiaLibro.IdCopia
+		JOIN Book ON CopiaLibro.LibroIdLibro = Book.id
+		JOIN Author ON Book.author = Author.id
+		WHERE Reserva.UsuarioIdU = ?
+	""", (user.id,))
+
+	#Obtener el historial de lectura del usuario
+	cur.execute("""
+		SELECT Book.title, Author.name
+		FROM HistorialLectura
+		JOIN Book ON HistorialLectura.LibroIdLibro = Book.id
+		JOIN Author ON Book.author = Author.id
+		WHERE HistorialLectura.UsuarioIdU = ?
+	""", (user.id,))
+
+	historial_lectura = cur.fetchall()
+
+	con.close()
+
+	return render_template('perfil.html', user=user, libros_en_reserva=libros_en_reserva, historial_lectura=historial_lectura)
+
 
 @app.route('/reserve')
 def reserve_book():
